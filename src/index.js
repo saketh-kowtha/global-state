2fe9b0127cf4a320564c20632ccdef0a7401e671
Saketh Kowtha <saketh.kowtha@thoughtworks.com>
2021-07-27 23:37:24 +0530

    Saketh | Added index.js file

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import combineReducers from "./lib/combineReducers";
+import connect from "./lib/connect";
+import Provider from "./lib/provider";
+import useDispatch from "./lib/useDispatch";
+import useSelector from "./lib/useSelector";
+
+export { combineReducers, connect, Provider, useDispatch, useSelector };
